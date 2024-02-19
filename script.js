47f131a1abc4871cab23f623fcedca2fa24bb4dd
Fedor <fkalit@evvve.net>
2024-02-19 14:28:58 +0300

    fix

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+//  Функция renderTree() отображает определение дерева, введенное пользователем, на веб-странице.
+//  Она получает ввод пользователя, парсит его, а затем отображает результат в элементе документа.
 function renderTree() {
   const input = document.getElementById('treeInput').value.trim();
   const treeOutput = document.getElementById('treeOutput');
-  
+
   if (input === '') {
       treeOutput.textContent = 'Пожалуйста, введите определение дерева.';
       return;
   }
-  
+
   try {
       const tree = parseTree(input);
       const treeStr = visualizeTree(tree);
@@ -16,6 +18,7 @@ function renderTree() {
   }
 }
 
+//   Функция parseTree(input) парсит строковое представление дерева и преобразует его в объект дерева.
 function parseTree(input) {
   const stack = [];
   let currentNode = null;
@@ -29,7 +32,7 @@ function parseTree(input) {
     const char = input[i];
     console.log('Текущий символ:', char);
     console.log('Текущий узел перед обновлением:', currentNode);
-  
+
     if (char === '(') {
       openBrackets++;
       if (!rootNode) {
@@ -42,7 +45,7 @@ function parseTree(input) {
         currentNode = newNode;
         currentNum = '';
       }
-    } 
+    }
     else if (char === ')') {
       closeBrackets++;
       if (currentNum !== '') {
@@ -72,14 +75,12 @@ function parseTree(input) {
       currentNum += char;
     }
   }
-  
+
   if (stack.length > 0) {
     console.error('Неправильное определение дерева: неверное количество открывающих скобок');
     console.log('Открывающих скобок:', openBrackets, 'Закрывающих скобок:', closeBrackets);
     return null;
   }
-  
-  
 
   if (currentNum !== '') {
     currentNode.children.push({ value: parseInt(currentNum), children: [] });
@@ -95,12 +96,13 @@ function parseTree(input) {
   return rootNode;
 }
 
+// ункция renderTree() - отображает дерево на веб-странице.
 function visualizeTree(tree, depth = 0) {
   let treeStr = '';
 
   if (tree) {
     if (!isNaN(tree.value) && tree.value !== null) {
-      treeStr += ' '.repeat(depth * 4) + tree.value + '\n';
+      treeStr += ' '.repeat(depth * 8) + tree.value + '\n';
     }
 
     tree.children.forEach(child => {
