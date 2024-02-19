585d32cf14b8541fd226081bf17adf5110f5dd3b
Fedor <fkalit@evvve.net>
2024-02-19 12:57:05 +0300

    add psrseTree

--- script.js
+++ script.js
@@ -17,11 +17,74 @@ function renderTree() {
 }
 
 function parseTree(input) {
+  const stack = [];
+  let currentNode = null;
+  let currentNum = '';
+  let rootNode = null;
+  console.log("rootNode",rootNode);
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
 
+    if (char === '(') {
+        if (!rootNode) {
+            rootNode = { value: null, children: [] };
+            currentNode = rootNode;
+            console.log("currentNode",currentNode)
+        } else if (currentNode) {
+            const newNode = { value: parseInt(currentNum), children: [] };
+            currentNode.children.push(newNode);
+            stack.push(currentNode);
+            console.log("currentNode",currentNode)
+            currentNode = newNode;
+            currentNum = '';
+        }
+    } else if (char === ')') {
+        if (currentNum !== '') {
+            currentNode.children.push({ value: parseInt(currentNum), children: [] });
+            currentNum = '';
+        }
+        if (stack.length === 0) {
+            console.error('Неправильное определение дерева: неверное количество закрывающих скобок');
+            return null;
+        }
+        currentNode = stack.pop();
+    } else if (char === ' ') {
+        if (currentNum !== '') {
+            currentNode.children.push({ value: parseInt(currentNum), children: [] });
+            currentNum = '';
+        }
+    } else {
+        currentNum += char;
+    }
+  }
+
+  if (currentNum !== '') {
+      currentNode.children.push({ value: parseInt(currentNum), children: [] });
+  }
+
+  if (stack.length !== 0) {
+      console.error('Неправильное определение дерева: неверное количество открывающих скобок');
+      return null;
+  }
+
+  if (!rootNode) {
+      console.error('Неправильное определение дерева: отсутствует корень');
+      return null;
+  }
+console.log("rootNode",rootNode);
+  return rootNode;
 }
 
 
-function visualizeTree(tree) {
+function visualizeTree(tree, depth = 0) {
+  let treeStr = '';
 
+  if (tree) {
+      treeStr += ' '.repeat(depth * 4) + tree.value + '\n';
+      tree.children.forEach(child => {
+          treeStr += visualizeTree(child, depth + 1);
+      });
+  }
+
+  return treeStr;
 }
-
